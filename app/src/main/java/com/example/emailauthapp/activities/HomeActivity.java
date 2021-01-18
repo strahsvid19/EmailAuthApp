c3702eee463a7c1c4c9d6df433486df9794678e1
strahsvid <strahsvid@yandex.ru>
2021-01-18 11:38:43 +0300

    Created home home activity.

---
+++ app/src/main/java/com/example/emailauthapp/activities/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.emailauthapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.emailauthapp.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
