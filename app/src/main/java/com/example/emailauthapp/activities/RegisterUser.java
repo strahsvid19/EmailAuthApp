92a922413d8b9bf56ca3c55dfbc317765636c979
strahsvid <strahsvid@yandex.ru>
2021-01-19 12:02:07 +0300

    added a method for sign in user

--- app/src/main/java/com/example/emailauthapp/activities/RegisterUser.java
+++ app/src/main/java/com/example/emailauthapp/activities/RegisterUser.java
@@ -1,22 +1,40 @@
 package com.example.emailauthapp.activities;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+
 public class RegisterUser {
 
-    public static void createNewUser (FirebaseAuth mAuth, Context context, EditText email, EditText password) {
+    public static void createNewUser (Activity a, FirebaseAuth mAuth, Context context, EditText email, EditText password) {
 
         mAuth.createUserWithEmailAndPassword(email.getText().toString().trim(), password.getText().toString())
                 .addOnCompleteListener(task -> {
                    if (task.isSuccessful()) {
                        Toast.makeText(context, "Account has been created!", Toast.LENGTH_SHORT).show();
+                       a.startActivity(new Intent(context, MainActivity.class));
+                       a.finish();
                    } else {
                        Toast.makeText(context, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                    }
+                });
+    }
+
+    public static void signInUser (Activity a, FirebaseAuth mAuth, Context context, EditText email, EditText password) {
+
+        mAuth.signInWithEmailAndPassword(email.getText().toString().trim(), password.getText().toString().trim())
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        a.startActivity(new Intent(context, HomeActivity.class));
+                        a.finish();
+                    } else {
+                        Toast.makeText(context, "The email or password is not correct", Toast.LENGTH_SHORT).show();
+                    }
                 });
     }
 }
