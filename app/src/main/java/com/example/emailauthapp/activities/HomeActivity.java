8b8b0bfd43f4700aa871167a445e0c8198863c44
strahsvid <strahsvid@yandex.ru>
2021-01-19 12:00:36 +0300

    Added a homepage structure

--- app/src/main/java/com/example/emailauthapp/activities/HomeActivity.java
+++ app/src/main/java/com/example/emailauthapp/activities/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.emailauthapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.emailauthapp.databinding.ActivityHomeBinding;
 
@@ -13,5 +14,9 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
