f2f311bdf276e1f6f62ae963f2704e0d22408b6a
strahsvid <strahsvid@yandex.ru>
2021-01-22 14:35:25 +0300

    Replaced all methods

--- app/src/main/java/com/example/emailauthapp/validators/Validators.java
+++ app/src/main/java/com/example/emailauthapp/validators/Validators.java
@@ -6,15 +6,23 @@ import android.widget.EditText;
 public class Validators {
 
     public static boolean validateInput (EditText email, EditText password, EditText confirmPassword) {
+        return emailValidate(email) && passwordValidate(password, confirmPassword);
+    }
 
-        if (email.getText().toString().trim().isEmpty()) {
+    public static boolean emailValidate (EditText email) {
+        if (!Patterns.EMAIL_ADDRESS.matcher(email.getText().toString().trim()).matches()) {
+            email.setError("Incorrect email address");
+            email.requestFocus();
+            return false;
+        } else if (email.getText().toString().isEmpty()) {
             email.setError("Empty email address");
             email.requestFocus();
             return false;
-        } else if (!emailValidate(email)) {
-            return false;
         }
+        return true;
+    }
 
+    public static boolean passwordValidate (EditText password, EditText confirmPassword) {
         if (password.length() < 6) {
             password.setError("Min length of the password is 6 letters");
             password.requestFocus();
@@ -22,16 +30,6 @@ public class Validators {
         } else if (!password.getText().toString().equals(confirmPassword.getText().toString())) {
             confirmPassword.setError("Different passwords");
             confirmPassword.requestFocus();
-            return false;
-        }
-
-        return true;
-    }
-
-    public static boolean emailValidate (EditText email) {
-        if (!Patterns.EMAIL_ADDRESS.matcher(email.getText().toString().trim()).matches()) {
-            email.setError("Incorrect email address");
-            email.requestFocus();
             return false;
         }
         return true;
